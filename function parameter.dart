ea1f35ae43340b886c1837bd990ee0ecd0883752
rafiqi <muhammadrafiqi171@gmail.com>
2024-10-12 22:29:49 +0700

    Create function parameter.dart

---
+++ function parameter.dart
@@ -0,0 +1,8 @@
+INI ADALAH CONTOH PENGGUNAAN FUNCTION PARAMETER
+ void halo(String namaAwal, String namaAkhir){
+   print('halo $namaAwal $namaAkhir');
+ }
+
+ void main(){
+   halo('Muhammad', 'Rafiqi');
+ }
